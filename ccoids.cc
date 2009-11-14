c2d6880fe7ca7d843073c02f4ab683befac406f1
Adam Sampson <ats@offog.org>
2009-11-14 12:16:09 +0000

    Locations, and migration between them. (No viewers yet.)
    git-svn-id: https://projects.cs.kent.ac.uk/projects/cosmos/svn/demos/trunk/ccoids@1063 e28804a8-3bd4-eee1-e388-d56375b66714

--- ccoids.cc
+++ ccoids.cc
@@ -16,10 +16,10 @@
 
 using namespace std;
 
-// FIXME no wrapping at the moment!
-
-const int BIRDS = 300;
-const float VIEW_RADIUS = 0.2;
+const int BIRDS = 1000;
+const int WIDTH_LOCATIONS = 6;
+const int HEIGHT_LOCATIONS = 4;
+const float VIEW_RADIUS = 0.5;
 const float ATTRACT_FACTOR = 0.02;
 const float AVOID_RADIUS = 0.01;
 const float AVOID_FACTOR = 0.25;
@@ -65,12 +65,37 @@ public:
 		return *this;
 	}
 
+	bool operator==(const Vector<ELEMENT>& b) const {
+		return (x_ == b.x_) && (y_ == b.y_);
+	}
+
 	ELEMENT mag2() const {
 		return (x_ * x_) + (y_ * y_);
 	}
 
 	ELEMENT x_, y_;
 };
+
+float rand_float() {
+	return rand() / (1.0 * RAND_MAX);
+}
+
+int oversign(float v) {
+	if (v < 0) {
+		return -1;
+	} else if (v > 1) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
+
+const Vector<int> DIRECTIONS[] = {
+	Vector<int>(-1, -1), Vector<int>(0, -1), Vector<int>(1, -1),
+	Vector<int>(-1,  0),                     Vector<int>(1,  0),
+	Vector<int>(-1,  1), Vector<int>(0,  1), Vector<int>(1,  1)
+};
+const int NUM_DIRECTIONS = 8;
 
 class AgentInfo {
 public:
@@ -83,19 +108,48 @@ public:
 	Vector<float> pos_, vel_;
 };
 
+class Location;
 typedef map<int, AgentInfo> AIMap;
-class World {
+typedef map<int, Shared<Location> *> LocMap;
+
+class Location {
 public:
-	World() : id_counter_(0) {
+	Location(int id) : id_(id), id_counter_(0) {
 	}
 
-	void enter(AgentInfo& info) {
+	int id() const {
+		return id_;
+	}
+
+	void neighbour(int dir, Shared<Location> *loc) {
+		neighbours_[dir] = loc;
+	}
+
+	Shared<Location> *enter(AgentInfo& info) {
 		info.local_id_ = id_counter_++;
-		infos_[info.local_id_] = info;
+		return update(info);
 	}
 
-	void update(AgentInfo& info) {
+	Shared<Location> *update(AgentInfo& info) {
 		infos_[info.local_id_] = info;
+
+		Vector<int> signs(oversign(info.pos_.x_), oversign(info.pos_.y_));
+		if (signs.x_ == 0 && signs.y_ == 0) {
+			// Stay here.
+			return NULL;
+		} else {
+			// Go to a neighbour.
+			int dir = -1;
+			for (int i = 0; i < NUM_DIRECTIONS; ++i) {
+				if (DIRECTIONS[i] == signs) {
+					dir = i;
+					break;
+				}
+			}
+			info.pos_ -= DIRECTIONS[dir];
+			infos_.erase(info.local_id_);
+			return neighbours_[dir];
+		}
 	}
 
 	void look(AIMap::const_iterator& begin,
@@ -106,21 +160,70 @@ public:
 	}
 
 private:
+	int id_;
 	int id_counter_;
 	AIMap infos_;
+	LocMap neighbours_;
+};
+
+// FIXME Have a data type for this instead?
+int loc_id(int x, int y) {
+	return (y * WIDTH_LOCATIONS) + x;
+}
+
+class World {
+public:
+	World() {
+	}
+
+	~World() {
+		for (LocMap::iterator it = locations_.begin(); it != locations_.end(); ++it) {
+			delete it->second;
+		}
+	}
+
+	// The World owns the location after this.
+	void add(Context& ctx, Shared<Location> *loc) {
+		Claim<Location> c(ctx, *loc);
+		locations_[c->id()] = loc;
+	}
+
+	Shared<Location> *get(int id) {
+		return locations_[id];
+	}
+
+private:
+	LocMap locations_;
 };
 
 class Boid : public Activity {
 public:
-	Boid(AgentInfo info, Shared<World>& world, Barrier& bar)
-		: info_(info), world_(world), bar_(bar) {
+	Boid(AgentInfo info, Shared<Location> *loc, Barrier& bar)
+		: info_(info), loc_(loc), bar_(bar) {
+	}
+
+	void do_update(Context &ctx, bool enter) {
+		while (true) {
+			Claim<Location> c(ctx, *loc_);
+
+			Shared<Location> *new_loc;
+			if (enter) {
+				new_loc = c->enter(info_);
+			} else {
+				new_loc = c->update(info_);
+			}
+
+			if (new_loc == NULL) {
+				return;
+			} else {
+				loc_ = new_loc;
+				enter = true;
+			}
+		}
 	}
 
 	void run(Context& ctx) {
-		{
-			Claim<World> c(ctx, world_);
-			c->enter(info_);
-		}
+		do_update(ctx, true);
 
 		float angle = 0.0;
 
@@ -133,7 +236,7 @@ public:
 			AIVector view;
 
 			{
-				Claim<World> c(ctx, world_);
+				Claim<Location> c(ctx, *loc_);
 				AIMap::const_iterator it, end;
 				c->look(it, end);
 
@@ -200,29 +303,14 @@ public:
 			}
 
 			info_.pos_ += info_.vel_;
-			while (info_.pos_.x_ < -0.0) {
-				info_.pos_.x_ += 1.0;
-			}
-			while (info_.pos_.x_ > 1.0) {
-				info_.pos_.x_ -= 1.0;
-			}
-			while (info_.pos_.y_ < -0.0) {
-				info_.pos_.y_ += 1.0;
-			}
-			while (info_.pos_.y_ > 1.0) {
-				info_.pos_.y_ -= 1.0;
-			}
 
-			{
-				Claim<World> c(ctx, world_);
-				c->update(info_);
-			}
+			do_update(ctx, false);
 		}
 	}
 
 private:
 	AgentInfo info_;
-	Shared<World>& world_;
+	Shared<Location> *loc_;
 	Barrier& bar_;
 };
 
@@ -260,10 +348,12 @@ public:
 
 		ctx.spawn(new EventHandler);
 
-		int size = 400;
+		int scale = 500 / HEIGHT_LOCATIONS;
 
 		SDL_WM_SetCaption("ccoids", "ccoids");
-		SDL_Surface *display = SDL_SetVideoMode(size, size, 32, SDL_DOUBLEBUF);
+		SDL_Surface *display = SDL_SetVideoMode(WIDTH_LOCATIONS * scale,
+		                                        HEIGHT_LOCATIONS * scale,
+		                                        32, SDL_DOUBLEBUF);
 
 		while (true) {
 			bar_.sync(ctx); // Phase 1
@@ -271,17 +361,25 @@ public:
 			SDL_FillRect(display, NULL, 0);
 			uint32_t *pixels = (uint32_t *) display->pixels;
 
-			{
-				Claim<World> c(ctx, world_);
+			for (int x = 0; x < WIDTH_LOCATIONS; ++x) {
+				for (int y = 0; y < HEIGHT_LOCATIONS; ++y) {
+					Shared<Location> *loc;
+					{
+						Claim<World> c(ctx, world_);
+						loc = c->get(loc_id(x, y));
+					}
+					Claim<Location> c(ctx, *loc);
 
-				AIMap::const_iterator it, end;
-				c->look(it, end);
+					AIMap::const_iterator it, end;
+					c->look(it, end);
 
-				for (; it != end; ++it) {
-					const AgentInfo& info = it->second;
+					for (; it != end; ++it) {
+						const AgentInfo& info = it->second;
 
-					Vector<int> p(info.pos_ * (float) size);
-					pixels[(p.y_ * size) + p.x_] = 0xFFFFFF;
+						Vector<int> p(info.pos_ * (float) scale);
+						p += Vector<int>(x, y) * scale;
+						pixels[(p.y_ * scale * WIDTH_LOCATIONS) + p.x_] = 0xFFFFFF;
+					}
 				}
 			}
 
@@ -302,17 +400,53 @@ class Ccoids : public Activity {
 		cout << "ccoids starting" << endl;
 
 		Barrier bar(ctx, 1);
-		Shared<World> world(ctx, new World);
+
+		// Set up the world.
+		// We can do this privately before sharing it.
+		World *w = new World;
+		for (int x = 0; x < WIDTH_LOCATIONS; ++x) {
+			for (int y = 0; y < HEIGHT_LOCATIONS; ++y) {
+				w->add(ctx, new Shared<Location>(ctx, new Location(loc_id(x, y))));
+			}
+		}
+		// This is slightly more complicated than in occam, because we
+		// can't create all the interfaces (for the neighbours) before
+		// spawning the servers -- so we need this second step.
+		for (int x = 0; x < WIDTH_LOCATIONS; ++x) {
+			for (int y = 0; y < HEIGHT_LOCATIONS; ++y) {
+				Shared<Location> *loc = w->get(loc_id(x, y));
+				Claim<Location> c(ctx, *loc);
+				for (int i = 0; i < NUM_DIRECTIONS; ++i) {
+					Vector<int> dir = DIRECTIONS[i];
+					int nx = (dir.x_ + x + WIDTH_LOCATIONS) % WIDTH_LOCATIONS;
+					int ny = (dir.y_ + y + HEIGHT_LOCATIONS) % HEIGHT_LOCATIONS;
+					Shared<Location> *n = w->get(loc_id(nx, ny));
+					c->neighbour(i, n);
+				}
+			}
+		}
+		Shared<World> world(ctx, w);
+
 		{
 			Context f(ctx);
 
 			for (int id = 0; id < BIRDS; id++) {
+				Vector<float> pos(rand_float() * WIDTH_LOCATIONS,
+				                  rand_float() * HEIGHT_LOCATIONS);
+				Vector<int> pos_loc(pos);
+
+				Shared<Location> *loc;
+				{
+					Claim<World> c(ctx, world);
+					loc = c->get(loc_id(pos_loc.x_, pos_loc.y_));
+				}
+
 				AgentInfo info;
 				info.id_ = id;
-				info.pos_ = Vector<float>(rand() / (1.0 * RAND_MAX),
-				                          rand() / (1.0 * RAND_MAX));
+				info.pos_ = Vector<float>(pos.x_ - pos_loc.x_,
+				                          pos.y_ - pos_loc.y_);
 
-				f.spawn(new Boid(info, world, bar.enroll()));
+				f.spawn(new Boid(info, loc, bar.enroll()));
 			}
 
 			f.spawn(new Display(world, bar));
